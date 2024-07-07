4d2bad0e31edac0b45b993d7d1cbdd83b576f4a7
Nishant <109360944+nishant11345@users.noreply.github.com>
2024-07-07 19:34:48 +0530

    commit

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const emailRoutes = require('./routes/emailRoutes');
-require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', emailRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGO_URI = 'mongodb+srv://ojhasumit0428:xiaMPSUj6Xmv5SVX@cluster0.f800cym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = 8000;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => console.error(error));
